fcf0b3c1aed38785f36276257ff982c3e34be425
Engel Garcia <engel.ym@gmail.com>
2025-08-03 15:33:41 -0500

    Actualizacion de Js, se agrego funciones

--- app.js
+++ app.js
@@ -1,1 +1,45 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+const amigos = [];
+
+function agregarAmigo(){
+    const input = document.getElementById("amigo");
+    const amigo =input.value.trim();
+    console.log (amigo)
+
+    if (amigo === ""){
+        alert ("Ingrese un nombre valido");
+        return;
+    }
+
+    if (amigos.includes(amigo)){
+        alert ("Nombre ya fue ingresado");
+        return;
+    }
+    amigos.push(amigo);
+    input.value ="";
+
+    MostrarAmigos();
+}
+function MostrarAmigos() {
+    const lista = document.getElementById ("listaAmigos");
+    lista.innerHTML ="";
+
+    amigos.forEach(amigo => {
+        const li = document.createElement("li");
+        li.textContent= amigo;
+        lista.appendChild(li);
+    })
+    
+}
+
+function sortearAmigo(){
+    if (amigos.length === 0 ){
+        alert ("Necesitas al 2 personas para el sorteo");
+        return;
+    }
+
+    const indice = Math.floor(Math.random()*amigos.length);
+    const elegido = amigos[indice];
+    document.getElementById("resultado").textContent=`el amigo secreto es: 🎁 ${elegido} 🎁`;
+}
